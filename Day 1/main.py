b4817791a43c7349e12be3f20722a5456ce9cd43
Rayson Su <rsu@connect.kellettschool.com>
2023-12-02 14:21:11 +0800

    Updates day 1 to use a template with testing

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,3 +1,6 @@
+OUTPUT_TYPE = int
+
+
 def get_digit(string: str, replacer: list[str], reverse: bool) -> str:
     for index in range(len(string)):
         target_index: int = (-index - 1) if reverse else index
@@ -15,8 +18,8 @@ def get_digit(string: str, replacer: list[str], reverse: bool) -> str:
 def main_part_1(inp: list[str]) -> int:
     ret: int = 0
     for line in inp:
-        first: str = get_digit(line, replacer=[], reverse=False)
-        last: str = get_digit(line, replacer=[], reverse=True)
+        first: str = get_digit(line, [], False)
+        last: str = get_digit(line, [], True)
         ret += int(first + last)
     return ret
 
@@ -35,10 +38,39 @@ def main_part_2(inp: list[str]) -> int:
 
 
 def main() -> None:
-    file_location: str = "Day 1/input.txt"
+    test_input: str = """two1nine
+eightwo1three
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
+    test_input_parsed: list[str] = test_input.split("\n")
+    test_output_part_1_expected: OUTPUT_TYPE = 220
+    test_output_part_2_expected: OUTPUT_TYPE = 281
+
+    file_location: str = "python/Advent of Code/2023/Day 1/input.txt"
     input_file: list[str] = open(file_location, "r").readlines()
-    print(main_part_1(input_file))
-    print(main_part_2(input_file))
+
+    test_output_part_1: OUTPUT_TYPE = main_part_1(test_input_parsed)
+    test_output_part_2: OUTPUT_TYPE = main_part_2(test_input_parsed)
+
+    if test_output_part_1_expected == test_output_part_1:
+        print(f"Part 1: {main_part_1(input_file)}")
+    else:
+        print(f"Part 1 testing error: ")
+        print(f"Test input: {test_input}")
+        print(f"Expected output: {test_output_part_1_expected}")
+        print(f"Got: {test_output_part_1}")
+        print()
+
+    if test_output_part_2_expected == test_output_part_2:
+        print(f"Part 2: {main_part_2(input_file)}")
+    else:
+        print(f"Part 2 testing error: ")
+        print(f"Test input: {test_input}")
+        print(f"Expected output: {test_output_part_2_expected}")
+        print(f"Got: {test_output_part_2}")
 
 
 if __name__ == "__main__":
