610d214b856dc74a8fe2713b6ec7f9f9ce240d85
Rayson Su <rsu@connect.kellettschool.com>
2023-12-06 13:47:58 +0800

    Day 6 input path fix

--- Day 6/main.py
+++ Day 6/main.py
@@ -42,7 +42,7 @@ Distance:  9  40  200"""
     test_output_part_1_expected: OUTPUT_TYPE = 288
     test_output_part_2_expected: OUTPUT_TYPE = 71503
 
-    file_location: str = "python/Advent of Code/2023/Day 6/input.txt"
+    file_location: str = "Day 6/input.txt"
     input_file: list[str] = open(file_location, "r").readlines()
 
     test_output_part_1: OUTPUT_TYPE = main_part_1(test_input_parsed)
